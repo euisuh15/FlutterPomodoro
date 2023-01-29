e57f05365fa6700d6544e7ad8d61b078e6b910b8
euisuh <esj@karty.qa>
2023-01-29 16:14:19 +0300

    Create progressFraction for round and goal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pomodoro/ui/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pomodoro/ui/page/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,15 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pomodoro',
       theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
         primaryColor: const Color(0xFFE7626C),
         dividerColor: const Color(0xFFF4EDDB),
         backgroundColor: const Color(0xFFFFFFFE),
         disabledColor: const Color(0xFF232B55),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
         cardColor: const Color(0xFFF4EDDB),
       ),
       home: const HomeScreen(),
